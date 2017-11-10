114c8664f5e749118b405cdd45ec034e3d392710
Ilyas <ihamdi@ihamdi-imac.local>
2017-11-10 16:15:24 -0500

    fix image_id

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 data "aws_availability_zones" "all" {
-  
+
 }
 
 variable "server_port" {
@@ -27,8 +27,8 @@ resource "aws_security_group" "instance" {
 }
 
 resource "aws_launch_configuration" "example" {
-  ami           = "ami-40d28157"
-  instance_type = "t2.micro"
+  image_id               = "ami-40d28157"
+  instance_type          = "t2.micro"
   vpc_security_group_ids = ["${aws_security_group.instance.id}"]
  
   user_data = <<-EOF
