139d580415e60153b679f7f90c34acef875712a5
Ilyas <ihamdi@ihamdi-imac.local>
2017-11-10 16:36:28 -0500

    add a load balancer with its sec group

--- main.tf
+++ main.tf
@@ -26,6 +26,24 @@ resource "aws_security_group" "instance" {
   }
 }
 
+resource "aws_security_group" "elb" {
+  name = "tf-playground-elb"
+
+  ingress {
+    from_port    = 80
+    to_port      = 80
+    protocol     = "tcp"
+    cidr_blocks  = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port    = 0
+    to_port      = 00
+    protocol     = "-1"
+    cidr_blocks  = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_launch_configuration" "example" {
   image_id               = "ami-40d28157"
   instance_type          = "t2.micro"
@@ -45,6 +63,9 @@ resource "aws_launch_configuration" "example" {
 resource "aws_autoscaling_group" "example" {
   launch_configuration = "${aws_launch_configuration.example.id}"
   availability_zones = ["${data.aws_availability_zones.all.names}"]
+  
+  load_balancers = ["${aws_elb.example.name}"]
+  health_check_type = "ELB"
 
   min_size = 2
   max_size = 10
@@ -56,8 +77,30 @@ resource "aws_autoscaling_group" "example" {
   }
 }
 
-# output "public_ip" {
-#   value = "${aws_instance.example.public_ip}"
-# }
+resource "aws_elb" "example" {
+  name               = "tf-asg-example"
+  availability_zones = ["${data.aws_availability_zones.all.names}"]
+  security_groups    = ["${aws_security_group.elb.id}"]
+
+  listener {
+    lb_port = 80
+    lb_protocol = "http"
+    instance_port = "${var.server_port}"
+    instance_protocol = "http"
+  }
+
+  health_check {
+    healthy_treshold = 2
+    unhealthy_treshold = 2
+    timeout = 3
+    interval = 30
+    target = "HTTP:${var.server_port}/"
+  }
+}
+
+
+output "elb_dns_name" {
+  value = "${aws_elb.example.elb_dns_name}"
+}
 
 
